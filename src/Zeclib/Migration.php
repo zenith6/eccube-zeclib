5f42d193896b15ecc8a4b10d352759fca7f6b990
Seiji Nitta <zenith6@gmail.com>
2014-05-07 00:10:32 +0900

    マイグレーションにスキーマ操作に関するメソッドを追加

--- src/Zeclib/Migration.php
+++ src/Zeclib/Migration.php
@@ -73,12 +73,227 @@ abstract class Zeclib_Migration
      * @param $options array
      * @return MDB2_Driver_Manager_Common
      */
-    public function getMDB2()
+    protected function getMDB2()
     {
         $mdb2 = $this->query->conn;
         $mdb2->loadModule('Manager');
         $mdb2->loadModule('Reverse');
 
         return $mdb2;
+    }
+
+    /**
+     * テーブルを作成します。
+     *
+     * @param string $tableName
+     * @param array $def
+     * @throws Zeclib_MigrationException
+     * @see MDB2_Driver_Manager_Common::createTable()
+     */
+    protected function createTable($tableName, array $def)
+    {
+        $mdb2 = $this->getMDB2();
+        $result = $mdb2->createTable($tableName, $def['fields']);
+        if (PEAR::isError($result)) {
+            $message = $result->getMessage() . ' ' . $result->getDebugInfo();
+            throw new Zeclib_MigrationException($message, $result->getCode());
+        }
+
+        if (isset($def['indexes'])) {
+            foreach ($def['indexes'] as $name => $index) {
+                $this->createIndex($tableName, $name, $index);
+            }
+        }
+
+        if (isset($def['constraints'])) {
+            foreach ($def['constraints'] as $name => $constraint) {
+                $this->createConstraint($tableName, $name, $constraint);
+            }
+        }
+
+        $message = sprintf('Table "%s" was created.', $tableName);
+        $this->log($message);
+    }
+
+    /**
+     * テーブルを削除します。
+     *
+     * @param string $tableName
+     * @param array $def
+     * @throws Zeclib_MigrationException
+     * @see MDB2_Driver_Manager_Common::dropTable()
+     */
+    protected function dropTable($tableName, array $def)
+    {
+        if (isset($def['constraints'])) {
+            foreach ($def['constraints'] as $name => $constraint) {
+                $this->dropConstraint($tableName, $name, $constraint);
+            }
+        }
+
+        if (isset($def['indexes'])) {
+            foreach ($def['indexes'] as $name => $index) {
+                $this->dropIndex($tableName, $name, $index);
+            }
+        }
+
+        $mdb2 = $this->getMDB2();
+        $result = $mdb2->dropTable($tableName);
+        if (PEAR::isError($result)) {
+            throw new Zeclib_MigrationException($result->getMessage() . ' ' . $result->getDebugInfo(), $result
+                ->getCode());
+        }
+
+        $message = sprintf('Table "%s" was dropped.', $tableName);
+        $this->log($message);
+    }
+
+    /**
+     * インデックスを作成します。
+     *
+     * @param string $tableName
+     * @param string $name
+     * @param array $def
+     * @throws Zeclib_MigrationException
+     * @see MDB2_Driver_Manager_Common::createIndex()
+     */
+    protected function createIndex($tableName, $name, array $def)
+    {
+        $mdb2 = $this->getMDB2();
+        $result = $mdb2->createIndex($tableName, $name, $def);
+        if (PEAR::isError($result)) {
+            $message = $result->getMessage() . ' ' . $result->getDebugInfo();
+            throw new Zeclib_MigrationException($message, $result->getCode());
+        }
+
+        $message = sprintf('Index "%s" was created to "%s".', $name, $tableName);
+        $this->log($message);
+    }
+
+    /**
+     * インデックスを削除します。
+     *
+     * @param string $tableName
+     * @param string $name
+     * @throws Zeclib_MigrationException
+     * @see MDB2_Driver_Manager_Common::dropIndex()
+     */
+    protected function dropIndex($tableName, $name)
+    {
+        $mdb2 = $this->getMDB2();
+        $result = $mdb2->dropIndex($tableName, $name);
+        if (PEAR::isError($result)) {
+            $message = $result->getMessage() . ' ' . $result->getDebugInfo();
+            throw new Zeclib_MigrationException($message, $result->getCode());
+        }
+
+        $message = sprintf('Index "%s" was created to "%s".', $name, $tableName);
+        $this->log($message);
+    }
+
+    /**
+     * 制約を作成します。
+     *
+     * @param string $tableName
+     * @param string $name
+     * @param array $def
+     * @throws Zeclib_MigrationException
+     * @see MDB2_Driver_Manager_Common::createConstraint()
+     */
+    protected function createConstraint($tableName, $name, array $def)
+    {
+        $mdb2 = $this->getMDB2();
+        $result = $mdb2->createConstraint($tableName, $name, $def);
+        if (PEAR::isError($result)) {
+            $message = $result->getMessage() . ' ' . $result->getDebugInfo();
+            throw new Zeclib_MigrationException($message, $result->getCode());
+        }
+
+        $message = sprintf('Constraint "%s" was created to "%s".', $name, $tableName);
+        $this->log($message);
+    }
+
+    /**
+     * 制約を削除します。
+     *
+     * @param string $tableName
+     * @param string $name
+     * @param bool $primaryKey
+     * @throws Zeclib_MigrationException
+     * @see MDB2_Driver_Manager_Common::dropConstraint()
+     */
+    protected function dropConstraint($tableName, $name, $primaryKey)
+    {
+        $mdb2 = $this->getMDB2();
+        $result = $mdb2->dropConstraint($tableName, $name, $primaryKey);
+        if (PEAR::isError($result)) {
+            $message = $result->getMessage() . ' ' . $result->getDebugInfo();
+            throw new Zeclib_MigrationException($message, $result->getCode());
+        }
+
+        $message = sprintf('Constraint "%s" was dropped from "%s".', $name, $tableName);
+        $this->log($message);
+    }
+
+    /**
+     * シーケンスを作成します。
+     *
+     * @param string $name
+     * @param string $start
+     * @throws Zeclib_MigrationException
+     * @see MDB2_Driver_Manager_Common::createSequence()
+     */
+    protected function createSequence($name, $start)
+    {
+        $mdb2 = $this->getMDB2();
+        $result = $mdb2->createSequence($name, $start);
+        if (PEAR::isError($result)) {
+            $message = $result->getMessage() . ' ' . $result->getDebugInfo();
+            throw new Zeclib_MigrationException($message, $result->getCode());
+        }
+
+        $message = sprintf('Sequence "%s" was created.', $name);
+        $this->log($message);
+    }
+
+    /**
+     * シーケンスを削除します。
+     *
+     * @param string $name
+     * @throws Zeclib_MigrationException
+     * @see MDB2_Driver_Manager_Common::dropSequence()
+     */
+    protected function dropSequence($name)
+    {
+        $mdb2 = $this->getMDB2();
+        $result = $mdb2->dropSequence($name);
+        if (PEAR::isError($result)) {
+            $message = $result->getMessage() . ' ' . $result->getDebugInfo();
+            throw new Zeclib_MigrationException($message, $result->getCode());
+        }
+
+        $message = sprintf('Sequence "%s" was dropped.', $name);
+        $this->log($message);
+    }
+
+    /**
+     * テーブルを変更します。
+     *
+     * @param string $tableName
+     * @param array $change
+     * @throws Zeclib_MigrationException
+     * @see MDB2_Driver_Manager_Common::alterTable()
+     */
+    protected function alterTable($tableName, array $change)
+    {
+        $mdb2 = $this->getMDB2();
+        $result = $mdb2->alterTable($tableName, $change);
+        if (PEAR::isError($result)) {
+            $message = $result->getMessage() . ' ' . $result->getDebugInfo();
+            throw new Zeclib_MigrationException($message, $result->getCode());
+        }
+
+        $message = sprintf('Table "%s" was altered.', $tableName);
+        $this->log($message);
     }
 }
